8de381bd9b878f850dbedf90bc97b745c1e6377d
Stacey <79818270+louyecats@users.noreply.github.com>
2023-04-26 11:26:33 -0400

    added Axios for API

--- pokemon_api/src/App.js
+++ pokemon_api/src/App.js
@@ -1,27 +1,39 @@
 import './App.css';
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-
   const [pokemon, setPokemon] = useState([]);
   useEffect(() => {
-      fetch('https://pokeapi.co/api/v2/pokemon/')
-          .then(response => response.json())
-          .then(response => setPokemon(response.results))
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/?limit=870')
+      .then(response => {
+      console.log(response);
+      setPokemon(response.data.results);
+    })
+    .catch(err => console.log(err));
   }, []);
+// This empty array forces useEffect to render ONLY when the component first renders
 
+//JSON useEffect
+// const [pokemon, setPokemon] = useState([]);
+// useEffect(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/')
+//         .then(response => response.json())
+//         .then(response => setPokemon(response.results))
+// }, []);
 
-  return (
+return (
     <div className="App">
       <h1>Pokemon API</h1>
       <form action="">
         <button>Fetch Pokemon</button>
-        </form>
-        <div>
-              {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
-                  return (<div key={index}>{pokemon.name}</div>)
-              })}
-          </div>
+      </form>
+      <div>
+        {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+          return (<div key={index}>{pokemon.name}</div>)
+        })}
+      </div>
     </div>
   );
 }
